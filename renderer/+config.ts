651dcd4eaf0630233b7ef5ac51b15888fda5758f
Dido (Christoph Poelt) <christoph.poelt@valiton.com>
2024-08-21 11:55:17 +0200

    create repro

--- renderer/+config.ts
+++ renderer/+config.ts
@@ -1,19 +1,18 @@
-import type { Config } from 'vike/types'
+import type { Config } from "vike/types";
 
 // https://vike.dev/config
 export default {
   // https://vike.dev/clientRouting
-  clientRouting: true,
+  clientRouting: false,
   // https://vike.dev/meta
   meta: {
     // Define new setting 'title'
     title: {
-      env: { server: true, client: true }
+      env: { server: true, client: true },
     },
     // Define new setting 'description'
     description: {
-      env: { server: true }
-    }
+      env: { server: true },
+    },
   },
-  hydrationCanBeAborted: true
-} satisfies Config
+} satisfies Config;
